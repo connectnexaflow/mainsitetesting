4d58ed7318220340b637647a77bdf36382cd70eb
connectnexaflow <connect.nexaflow@gmail.com>
2026-05-11 10:35:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -129,7 +129,7 @@
         observer.unobserve(entry.target);
       }
     });
-  }, { threshold: 0.12, rootMargin: '0px 0px -40px 0px' });
+}, { threshold: 0.08, rootMargin: '0px 0px -20px 0px' });
 
   targets.forEach(el => observer.observe(el));
 })();
